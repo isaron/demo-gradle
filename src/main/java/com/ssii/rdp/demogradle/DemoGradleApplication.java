e651064d877f18d402b9e42ff7a5ff874a1a32f8
huangch <isaron@sohu.com>
2019-01-18 15:09:03 +0800

    update gradle dependencies

--- src/main/java/com/ssii/rdp/demogradle/DemoGradleApplication.java
+++ src/main/java/com/ssii/rdp/demogradle/DemoGradleApplication.java
@@ -2,9 +2,16 @@ package com.ssii.rdp.demogradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoGradleApplication {
+	@RequestMapping("/")
+	public String index(){
+		return "Hello Gradle!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGradleApplication.class, args);
