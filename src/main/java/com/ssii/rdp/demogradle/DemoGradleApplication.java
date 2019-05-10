1175eab783f4eecbc3fa231f16e5d41bf5a40b37
huangch <isaron@sohu.com>
2019-05-10 16:56:30 +0800

    update Jenkinsfile: ci process

--- src/main/java/com/ssii/rdp/demogradle/DemoGradleApplication.java
+++ src/main/java/com/ssii/rdp/demogradle/DemoGradleApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoGradleApplication {
 	@RequestMapping("/")
 	public String index(){
-		return "Hello Gradle 6!";
+		return "Hello Gradle! 2019/5/10";
 	}
 
 	public static void main(String[] args) {
