afc3e04a5cda2f76ede914b9bc959d17d4c963bc
huangch <isaron@sohu.com>
2019-05-20 17:53:18 +0800

    update jib versions

--- src/main/java/com/ssii/rdp/demogradle/DemoGradleApplication.java
+++ src/main/java/com/ssii/rdp/demogradle/DemoGradleApplication.java
@@ -2,17 +2,11 @@ package com.ssii.rdp.demogradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class DemoGradleApplication {
-	@RequestMapping("/")
-	public String index(){
-		return "Hello Gradle! 2019/5/20";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGradleApplication.class, args);
 	}
