d79abcfa09229bd791c8b733013a87e4de864e7f
huangch <isaron@sohu.com>
2019-08-06 15:34:32 +0800

    testing ci process

--- src/main/java/com/ssii/rdp/demogradle/DemoController.java
+++ src/main/java/com/ssii/rdp/demogradle/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Kubernetes! develop111 branch";
+        return "Greetings from Kubernetes! testing branch";
     }
 }
